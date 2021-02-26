325b2edd99012b73b444cd017b0c1e60f77c0df8
Viktor Casubolo <viktor.casubolo98@gmail.com>
2021-02-26 17:56:20 +0100

    Aggiornamento Bonus

--- bonus/assets/js/main.js
+++ bonus/assets/js/main.js
@@ -1,10 +1,15 @@
+// dichiarazione di variabili
+
 var name = prompt("inserisci il tuo nome:", 'Mario');
 var surename = prompt('inserisci il tuo cognome:', 'Rossi');
 var color = prompt('inserisci il tuo colore preferito:', 'Verde');
-
 var password = name + surename + color + 21;
+var i = 0;
+var hoverRM = document.getElementById('hover');
 
-document.getElementById("pw").value = password;
+// document.getElementById("pw").value = password;
+
+// Funzioni
 
 function copyFunction() {
     var copyText = document.getElementById("pw");
@@ -12,3 +17,16 @@ function copyFunction() {
     document.execCommand("copy");
     alert("Copied the text: " + copyText.value);
   }
+
+function writeFunction(){
+  if (i < password.length){
+    document.getElementById("pw").value += password.charAt(i);
+    i++;
+    setTimeout(writeFunction, 130);
+  }
+}
+
+
+function removerHOVER(){
+  hoverRM.remove();
+}
